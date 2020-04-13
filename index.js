882cd1a4c3d133649a7364cdb91f68f2310f9c71
Lars Nelson <lnelson7820@gmail.com>
2020-04-13 10:33:21 -0700

    Added partials and implemented home page

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
+/**************
+ * NODE MODULES
+***************/
 // Important variables
 var express = require("express")
 var layouts = require("express-ejs-layouts")
 var app = express()
 
-// Make useful the things you brought in
+/****************************
+ * MIDDLEWARE AND/OR SETTINGS
+ ****************************/
 app.set("view engine", "ejs")
 app.use(layouts)
 app.use(express.static('static'))
 
-// Routes
-app.get('*', (req,res) => {
+/********
+ * ROUTES
+ ********/
+app.get('/', (req,res) => {
+    res.render('home')
+})
+// Wild card. Leave it for last.
+ app.get('*', (req,res) => {
     res.render('error')
 }) 
 
-// LISTEN FOR THE PORT
+/**********************
+ * SSSSSHHHHH... LISTEN
+ **********************/
 app.listen(3000, () => {
     console.log("ALRIGHT HERE WE GO")
 })
